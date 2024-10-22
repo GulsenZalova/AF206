67f69c6bb79e5548d82d075e8a4f13e15cac1dee
GulsenZalova <zalova0202@gmail.com>
2024-10-22 16:27:11 +0400

    js intro

---
+++ JS/JS-intro/assets/JS/index.js
@@ -0,0 +1,194 @@
+// console.log("Hello World")
+// alert("Hello")
+// prompt("Eded daxil edin")
+// confirm("Silinsinmi?")
+// console.error("Hello World")
+// console.warn("Hello World")
+
+
+// JS Variables
+// let , const ,var
+
+
+
+// let name="Gulshan"
+// const birthyear=2024
+// var surname="Zalova"
+
+
+// declare,redeclare,assing,reassign  - var
+
+
+// var surname="Eliyeva"
+
+// console.log(surname)
+
+
+
+// declare, assign,reassing
+
+// let name="Sabina"
+
+
+// name="Sabina"
+
+
+// console.log(name)
+
+
+// declare,assign
+
+
+// const birthyear
+
+//  birthyear=2025
+
+//  console.log(birthyear)
+
+
+
+
+
+// JS data types
+
+
+// String
+
+
+
+
+// let name=prompt("adinizi daxil edin")
+// let surname=prompt("soyadinizi daxil edin")
+// let fullname=`${surname} ${name}`
+
+// console.log(fullname)
+
+
+
+// naming convention
+// camel case, pascal case, snake case, kebab case
+
+
+// let minMaxNumber
+
+// let MinMaxNumber
+
+// let min_max_number
+
+// let min-max-
+
+
+
+
+// let num=20
+
+// console.log(typeof num)
+
+// let isStudent=true
+
+// console.log(typeof isStudent)
+
+
+// let name
+// let surname
+
+// console.log(name,surname)
+
+
+
+// type convertion
+
+// implicite, explicite
+
+
+
+// let x=10
+// let y=5
+// let c="salam"
+// let z=c+x
+// console.log(z)
+
+// console.log(typeof undefined)
+
+
+
+// let num=25
+
+
+// console.log(num)
+// console.log(String(num))
+// console.log(num.toString())
+
+// console.log("5" - 3)
+
+// let num1=Number(prompt("eded 1"))
+// let num2=Number(prompt("eded2"))
+// let text="55"
+
+// console.log(Number(text))
+
+
+// console.log(num1+num2)
+// console.log(typeof num1)
+
+
+// Object
+
+
+// let student={
+//     name:"Vusal",
+//     surname:"Eliyev",
+//     age:20,
+//     hobbies:["game","music"]
+// }
+
+
+// let student1={
+//     name:"Xeyal",
+//     surname:"Eliyev",
+//     age:20,
+//     hobbies:["game","music"]
+// }
+
+
+
+// const telebe="Rauf"
+// telebe="Kanan"
+
+// console.log(telebe)
+// object
+// const student2={
+//     name:"Rauf",
+//     surname:"Eliyev",
+//     age:20,
+//     hobbies:["game","music"]
+// }
+// // Array
+// let students=[student, student1,student2]
+// console.log(typeof students)
+// student2.name="Kanan"
+// console.log(students[students.length-1].name)
+
+
+// Primitiv : string,number,boolean,null, undifined
+// Referance: object
+
+
+// let num=10
+// // num+=10
+// num-=10
+// console.log(num)
+
+
+// let x=10
+// let y=10
+
+
+// console.log(xy)
+
+
+
+
+// console.log(10>5 && (20<10 || 2>3))
+
+console.log(!(2>5))
